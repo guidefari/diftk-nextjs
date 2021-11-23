20d9025943e050989d7730d75790caff114dd903
Guide Fari <guideg6@gmail.com>
2021-11-23 09:05:10 +0200

    fix: misplaced '=' sign

--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,13 @@
-import Layout from "../components/layout";
+import Layout from "../components/layout"
 import Link from "next/link"
-import { Link2 } from 'react-feather'
-import { GraphQLClient } from "graphql-request";
+import { Link2 } from "react-feather"
+import { GraphQLClient } from "graphql-request"
 
-const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
+const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT)
 
 export async function getStaticProps() {
-    const { projects } = await graphcms.request(
-      `
+  const { projects } = await graphcms.request(
+    `
       query MyQuery() {
         projects {
           name
@@ -18,13 +18,13 @@ export async function getStaticProps() {
         }
       }
     `
-    );
-  
-    return {
-      props: {
-        projects,
-      },
-    };
+  )
+
+  return {
+    props: {
+      projects,
+    },
+  }
 }
 
 function Projects({ projects }) {
@@ -32,40 +32,65 @@ function Projects({ projects }) {
     <Layout>
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap mb-4 -mx-4 sm:-m-4">
-
-        <div id="fabhac-project-card" className=""="max-w-md py-4 px-8 bg-secondary shadow-lg rounded-lg my-20 mx-auto">
+          <div
+            id="fabhac-project-card"
+            className="max-w-md px-8 py-4 mx-auto my-20 rounded-lg shadow-lg bg-secondary"
+          >
             <div className="flex justify-center -mt-16 md:justify-end">
-              <img className="object-cover w-20 h-20 border-2 rounded-full border-writing" src="/fabhac.png"/>
+              <img
+                className="object-cover w-20 h-20 border-2 rounded-full border-writing"
+                src="/fabhac.png"
+              />
             </div>
             <div>
-              <h2 className="text-3xl font-semibold ">Find A Book, Help A Child</h2>
-              <p className="mt-2 ">Started in 2015, FABHAC is an acronym for “FIND A BOOK HELP A CHILD.” It is an initiative aimed at improving the plight of children and adults living with a disability, and more particularly with down-syndrome. Due to the costs involved in joining libraries, many literate people living with disabilties are denied the right to access books.</p>
+              <h2 className="text-3xl font-semibold ">
+                Find A Book, Help A Child
+              </h2>
+              <p className="mt-2 ">
+                Started in 2015, FABHAC is an acronym for “FIND A BOOK HELP A
+                CHILD.” It is an initiative aimed at improving the plight of
+                children and adults living with a disability, and more
+                particularly with down-syndrome. Due to the costs involved in
+                joining libraries, many literate people living with disabilties
+                are denied the right to access books.
+              </p>
             </div>
             <div className="flex justify-end mt-4">
-              <Link  href="/fabhac"><a title='read more' className="text-xl font-medium text-highlight"><Link2/></a></Link>
+              <Link href="/fabhac">
+                <a
+                  title="read more"
+                  className="text-xl font-medium text-highlight"
+                >
+                  <Link2 />
+                </a>
+              </Link>
             </div>
           </div>
 
-        {projects.map(project => (
-            <div key={project.name} className="max-w-md px-8 py-4 mx-auto my-20 rounded-lg shadow-lg bg-secondary">
-            <div className="flex justify-center -mt-16 md:justify-end">
-              <img className="object-cover w-20 h-20 border-2 rounded-full border-writing" src={project.icon.url}/>
-            </div>
-            <div>
-              <h2 className="text-3xl font-semibold ">{project.name}</h2>
-              <p className="mt-2 ">{project.summary}</p>
-            </div>
-            {/* <div className="flex justify-end mt-4">
+          {projects.map(project => (
+            <div
+              key={project.name}
+              className="max-w-md px-8 py-4 mx-auto my-20 rounded-lg shadow-lg bg-secondary"
+            >
+              <div className="flex justify-center -mt-16 md:justify-end">
+                <img
+                  className="object-cover w-20 h-20 border-2 rounded-full border-writing"
+                  src={project.icon.url}
+                />
+              </div>
+              <div>
+                <h2 className="text-3xl font-semibold ">{project.name}</h2>
+                <p className="mt-2 ">{project.summary}</p>
+              </div>
+              {/* <div className="flex justify-end mt-4">
               <a target='_blank' href="https://www.fawcetts.co.zw/" className="text-xl font-medium text-highlight"><ExternalLink/></a>
             </div> */}
-          </div>
+            </div>
           ))}
         </div>
-
-        
       </div>
     </Layout>
-  );
+  )
 }
 
-export default Projects;
+export default Projects
